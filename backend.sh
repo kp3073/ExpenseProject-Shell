bd1ec5f09a2deaf60d890958c98f6a6b90b03ae3
keyurpatel <keyurpatel8288@gmail.com>
2024-05-27 16:28:30 -0400

    n

--- backend.sh
+++ backend.sh
@@ -1,27 +1,37 @@
-#
-dnf module disable nodejs -y
-dnf module enable nodejs:18 -y
+echo disabling old Nodjs and enable New Nodjs
+dnf module disable nodejs -y >> /tmp/expense.log
+dnf module enable nodejs:18 -y >> /tmp/expense.log
 
-dnf install nodejs -y
+echo Installing Nginx
+dnf install nodejs -y >> /tmp/expense.log
 
-useradd expense
-rm -rf /app
-mkdir /app 
+echo adding user 
+useradd expense >> /tmp/expense.log
+rm -rf /app >> /tmp/expense.log
+mkdir /app  >> /tmp/expense.log
 
-cp backend.service /etc/systemd/system/backend.service
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip 
+echo Copying backend service file 
+cp backend.service /etc/systemd/system/backend.service >> /tmp/expense.log
+
+echo downloading backend zip file 
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip >> /tmp/expense.log
 cd /app 
 
-unzip /tmp/backend.zip
+echo Unziping file 
+unzip /tmp/backend.zip >> /tmp/expense.log
+
+echo Installing NPM dependancies
+
+npm install >> /tmp/expense.log
+
+echo restarting services
+systemctl daemon-reload >> /tmp/expense.log
+
+systemctl enable backend >> /tmp/expense.log
+systemctl start backend >> /tmp/expense.log
+
+echo installing my sqld
+dnf install mysql -y >> /tmp/expense.log
 
 
-npm install 
-
-systemctl daemon-reload
-
-systemctl enable backend 
-systemctl start backend
-dnf install mysql -y 
-
-
-mysql -h 172.31.27.95 -uroot -pExpenseApp@1 < /app/schema/backend.sql 
+mysql -h 172.31.27.95 -uroot -pExpenseApp@1 < /app/schema/backend.sql  >> /tmp/expense.log
--- mysql.sh
+++ mysql.sh
@@ -1,9 +1,14 @@
-dnf module disable mysql -y 
+echo disable old mysql
+dnf module disable mysql -y >> /tmp/expense.log 
 
-cp mysql.repo /etc/yum.repos.d/mysql.repo
+echo coping repo file 
+cp mysql.repo /etc/yum.repos.d/mysql.repo >> /tmp/expense.log
 
-dnf install mysql-community-server -y
+echo Installing MySql
+dnf install mysql-community-server -y >> /tmp/expense.log
 
-systemctl enable mysqld
-systemctl start mysqld  
-mysql_secure_installation --set-root-pass ExpenseApp@1
+echo Starting MySql services
+
+systemctl enable mysqld >> /tmp/expense.log
+systemctl start mysqld  >> /tmp/expense.log
+mysql_secure_installation --set-root-pass ExpenseApp@1 >> /tmp/expense.log
