4ec24f1f5a88f0e1757aecce07f07209549bb3d7
keyurpatel <keyurpatel8288@gmail.com>
2024-05-27 16:22:45 -0400

    b

--- frontend.sh
+++ frontend.sh
@@ -1,11 +1,21 @@
-dnf install nginx -y 
-systemctl enable nginx 
-systemctl start nginx
-rm -rf /usr/share/nginx/html/* 
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip 
-cp expense.conf /etc/nginx/default.d/expense.conf 
+echo installing Nginx
+dnf install nginx -y  >> /tmp/expense.log
+
+echo Removing Old Nginx Contant
+rm -rf /usr/share/nginx/html/*  >> /tmp/expense.log
+
+echo downloading Expense App contant
+
+curl -s -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip >> /tmp/expense.log
+
+echo coping the contant
+cp expense.conf /etc/nginx/default.d/expense.conf >> /tmp/expense.log
+
+echo unziping contant
 cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip >> /tmp/expense.log
 
+echo restart Nginx service
+systemctl enable nginx >> /tmp/expense.log
 
-systemctl restart nginx 
+systemctl restart nginx >> /tmp/expense.log
