dcadf51e07300520322a18f46eb61f4be23e7e7e
kp3073 <keyurpatel8288@gmail.com>
2024-05-28 23:16:21 -0400

    frest

--- backend.sh
+++ backend.sh
@@ -1,29 +1,68 @@
 log_file=/tmp/expense.log
-dnf module disable nodejs -y &>>$log_file
 
-dnf module disable nodejs -y &>>$log_file
+if [ -z "$1" ]; then
+  echo Password Input Missing
+  exit
+fi
 
+MYSQL_ROOT_PASSWORD=$1
+
+echo -e "${color} Disable NodeJS default Version \e[0m"
+dnf module disable nodejs -y &>>$log_file
+status_check
+
+echo -e "${color} Enable NodeJS 18 Version \e[0m"
+dnf module enable nodejs:18 -y &>>$log_file
+status_check
+
+echo -e "${color} Install NodeJS \e[0m"
 dnf install nodejs -y &>>$log_file
+status_check
 
+echo -e "${color} Copy Backend Service File \e[0m"
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
+status_check
 
-useradd expense &>>$log_file
+id expense &>>$log_file
+if [ $? -ne 0 ]; then
+  echo -e "${color} Add Application User \e[0m"
+  useradd expense &>>$log_file
+  status_check
+fi
 
-mkdir /app &>>$log_file
+if [ ! -d /app ]; then
+  echo -e "${color} Create Application Directory \e[0m"
+  mkdir /app &>>$log_file
+  status_check
+fi
 
+echo -e "${color} Delete old Application Content \e[0m"
 rm -rf /app/* &>>$log_file
+status_check
 
+echo -e "${color} Download Application Content \e[0m"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>$log_file
+status_check
 
+echo -e "${color} Extract Application Content \e[0m"
 cd /app &>>$log_file
 unzip /tmp/backend.zip &>>$log_file
+status_check
 
+echo -e "${color} Download NodeJS Dependencies \e[0m"
 npm install &>>$log_file
+status_check
 
+echo -e "${color} Install MySQL Client to Load Schema \e[0m"
 dnf install mysql -y &>>$log_file
+status_check
 
-mysql -h 172.31.25.169 -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>log_file
+echo -e "${color} Load Schema \e[0m"
+mysql -h 172.31.25.169 -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
+status_check
 
+echo -e "${color} Starting Backend Service \e[0m"
 systemctl daemon-reload &>>$log_file
 systemctl enable backend &>>$log_file
 systemctl restart backend &>>$log_file
+status_check
